d0db9b4bf12d338eceb9f44ba3eaa052017feb04
vagrant <vagrant@homestead>
2017-12-04 06:51:00 +0000

    Fininsh layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+      <a href="{{ route('home') }}">
+        是那只孤飞的蝶 | 逆着风与岁月悠远 | 回眸迢迢一眼 | 不可追的春天
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>@yield('title', 'Sample')</title>
-</head>
-<body>
-	@yield('content')
-</body>
+	<head>
+		<title>@yield('title', 'Sample App')</title>
+		<link rel="stylesheet" href="/css/app.css">
+	</head>
+	<body>
+		@include('layouts._header')
+		<div class="container">
+			@yield('content')
+			@include('layouts._footer')
+		</div>	
+	</body>
 </html>
