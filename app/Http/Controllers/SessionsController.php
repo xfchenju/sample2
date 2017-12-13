bf589932c73b3127257699d7d59804f341ba1c1a
vagrant <vagrant@homestead>
2017-12-13 08:21:56 +0000

    Finish user edit, update, index and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,16 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
     public function create() {
     	return view('sessions.create');
     }
@@ -20,13 +30,11 @@ class SessionsController extends Controller
 
 		if (Auth::attempt($credentials, $request->has('remember'))) {
 			session()->flash('success', '欢迎回来！');
-			return redirect()->route('users.show', [Auth::user()]);
+			return redirect()->intended(route('users.show', [Auth::user()]));
 		} else {
 			session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
 			return redirect()->back();
 		}
-
-    	return;
     }
 
     public function destroy() {
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,12 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'stroe','index']
+        ]);
+    }
+
     public function create() {
     	return view('users.create');
     }
@@ -35,5 +41,49 @@ class UsersController extends Controller
     	session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
     	
     	return redirect()->route('users.show',[$user]);
+    }
+
+    public function edit(User $user) {
+        $this->authorize('update', $user);
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user, Request $request) {
+        $this->authorize('update', $user);
+        
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        if($user->name != $request->name){
+            $data['name'] = $request->name;
+        }
+        
+        if($request->password && $user->password != bcrypt($request->password)){
+            $data['password'] = bcrypt($request->password);
+        }
+        if(!empty($data)){
+            $user->update($data);
+            session()->flash('success', '个人资料更新成功！');    
+        }else{
+            session()->flash('success', '个人资料与原来相同！');    
+        }
+        
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function index() {
+        $users = User::paginate(10); 
+        return view('users.index', compact('users'));
+    }
+
+    public function destroy(User $user) {
+        
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '删除用户成功！');
+        return back();
     }
 }
